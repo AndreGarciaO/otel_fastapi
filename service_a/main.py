ce2aa8e84e4df9b57f3434fd8a66adaf9cacf433
AndreGarciaO <80695600+AndreGarciaO@users.noreply.github.com>
2026-03-31 12:31:57 -0600

    Delete service_a directory from root directory

--- service_a/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
-from app.observability import tracing  # importante
-from app.service_a.routes import router
-
-app = FastAPI()
-
-FastAPIInstrumentor.instrument_app(app)
-app.include_router(router)
